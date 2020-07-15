bfdb9daa3dd96453e675d01407271f40cf9cce67
Avisrur <nadavavisrur@gmail.com>
2020-07-15 15:34:57 +0300

    removed configs

--- config/tableau-config.js
+++ config/tableau-config.js
@@ -1,6 +1,4 @@
 module.exports = {
-  username: "omri@hirupert.com",
-  password: "!Z4eyvgt2P3c",
   siteName: "rupertdev966607",
   signinUrl: "https://10ax.online.tableau.com/api/3.7/auth/signin",
   workbooksOnSiteUrl: "http://10ax.online.tableau.com/api/3.7/sites/{SITE_ID}/workbooks",
