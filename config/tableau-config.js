9807a94c4d3c6322c0f39ae759932e2368c3ac1b
Avisrur <nadavavisrur@gmail.com>
2020-07-15 15:40:14 +0300

    removed sensetive

---
+++ config/tableau-config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  siteName: "rupertdev966607",
+  signinUrl: "https://10ax.online.tableau.com/api/3.7/auth/signin",
+  workbooksOnSiteUrl: "http://10ax.online.tableau.com/api/3.7/sites/{SITE_ID}/workbooks",
+  viewsOnWorkbooksUrl: "http://10ax.online.tableau.com/api/3.7/sites/{SITE_ID}/workbooks/{WORKBOOK_ID}",
+  viewDataUrl: "http://10ax.online.tableau.com/api/3.7/sites/{SITE_ID}/views/{VIEW_ID}/data",
+};
